14a83823221d7f672092440e02f540f3eb878069
akash chhetri <akash.chhetri1@gmail.com>
2021-03-29 11:15:05 +1100

    new article routes controller and view added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,23 @@ class ArticlesController < ApplicationController
   def index 
     @articles = Article.all
   end
+
+  def new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
+
+  end
+
+  private
+  
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
