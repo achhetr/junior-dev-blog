36eb03d4656f211e4cb1bb3f22a854621337b02b
akash chhetri <akash.chhetri1@gmail.com>
2021-04-06 12:17:33 +1000

    user updated using form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,20 @@
 class UsersController < ApplicationController
+  before_action :set_user only: [:edit, :update]
+
   def new
     @user = User.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Your account information was successfully updated"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
   end
 
   def create
@@ -13,8 +27,13 @@ class UsersController < ApplicationController
     end
   end
 
+
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
