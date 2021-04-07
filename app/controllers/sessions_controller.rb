4cea2ef4188b84e6062e5b6af356d5a953f11762
akash chhetri <akash.chhetri1@gmail.com>
2021-04-07 10:22:00 +1000

    login and logout added in the session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,28 @@
 class SessionsController < ApplicationController 
   def new
-  
   end
 
   def create
+    user = User.find_by(email: user_params[:email].downcase)
+    if user && user.authenticate(user_params[:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Logged in successfully"
+      redirect_to user
+    else
+      flash.now[:alert] = "There was something wrong with your login detail"
+      render 'new'
+    end
   end
 
   def destroy
-  
+    session[:user_id] = nil
+    flash[:notice] = "Logged out successfully"
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:session).permit(:email, :password)
   end
 end
