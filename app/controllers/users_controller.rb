aacd794b62e04e3f4c4de17e43b20e1155331e54
akash chhetri <akash.chhetri1@gmail.com>
2021-04-07 16:12:06 +1000

    admin user functionality added to the app

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
 
   def destroy
     @user.destroy
-    session[:user_id] = nil
+    session[:user_id] = nil if @user == current_user
     flash[:notice] = "Account and all associated articles successfully deleted"
     redirect_to articles_path
   end
@@ -56,7 +56,7 @@ class UsersController < ApplicationController
   end
 
   def same_user
-    unless @user == current_user
+    if @user == current_user && !current_user.admin?
       flash[:alert] = "You are not authorized for this action"
       redirect_to @user
     end
